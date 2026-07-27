5782f580b040b8df97d4618bfc17e647c29e3dc1
bimannandib <bimannandib@gmail.com>
2026-07-27 13:09:26 +0530

    Use Neon PostgreSQL

--- backend/src/db/db_postgre.js
+++ backend/src/db/db_postgre.js
@@ -1,20 +1,40 @@
+// import { Pool } from "pg";
+
+// const pool = new Pool({
+//     user: process.env.DB_USER,
+//     host: process.env.DB_HOST,
+//     database: process.env.DB_DATABASE,
+//     password: process.env.DB_PASSWORD,
+//     port: process.env.DB_PORT
+// })
+
+// pool.connect((error) => {
+//     if (error) {
+//         console.error('Connection error', error.stack)
+        
+//     } else {
+//         console.log('Connected to PostgreSQL database')
+//     }
+// })
+
+// export { pool }
+
+
 import { Pool } from "pg";
 
 const pool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_DATABASE,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT
-})
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
 
-pool.connect((error) => {
-    if (error) {
-        console.error('Connection error', error.stack)
-        
-    } else {
-        console.log('Connected to PostgreSQL database')
-    }
-})
+pool.on("connect", () => {
+  console.log("Connected to PostgreSQL");
+});
 
-export { pool }
+pool.on("error", (err) => {
+  console.error("PostgreSQL Error:", err);
+});
+
+export { pool };
